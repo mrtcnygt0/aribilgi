883117e84d483db57c7098f6806eb13f213fdcc6
Mert <mertcanyigit54@outlook.com>
2026-04-11 19:42:23 +0300

    Add files via upload

--- ConsoleApps/MyProjects/Veriables/Program.cs
+++ ConsoleApps/MyProjects/Veriables/Program.cs
@@ -117,7 +117,8 @@ namespace Veriables
 
             // * Örnek içinde Döngülere (for) değinilmiştir.
 
-
+            /*
+            
             int s01 = 5;
             int s02 = 75;
             int s03 = 45;
@@ -140,14 +141,21 @@ namespace Veriables
             Array.Sort(ogrAdlari);
             // Alfabetik sıralanan diziyi tersine çevirme
             Array.Reverse(ogrAdlari);
+            // Array boyutunu değiştirme
+            Array.Resize(ref ogrAdlari, 10);
+            // Dizideki bu isimdeki verinin hangi indekste olduğunu çekmek
+            Array.IndexOf(ogrAdlari, "Mert");
+            // Çekilen değeri ekrana yazdırma
+            Console.WriteLine(Array.IndexOf(ogrAdlari, "Mert"));
 
-
+            
             // goto ile yapılan döngü:
             // sayac ile döngünün ilerlemesi ve dizi indexinin artarak yazılması sağlandı.
             int sayac = 0;
         Tekrar:
             Console.WriteLine((sayac+1) +". Öğrencinin ismi: " + ogrAdlari[sayac]);
             sayac++;
+
             if(sayac < ogrAdlari.Length) goto Tekrar;
 
 
@@ -159,7 +167,112 @@ namespace Veriables
 
             }
 
-            
+
+            sayac = 0;
+            int[] isimler = new int[10];
+            // Dizileri bu şekilde aktarmak, verileri değil verilerin ramde tutulduğu adresleri aktarır! Diziler referans tiplidir.
+            isimler = ogrNotlar;
+
+            isimler[0] = 25;
+            //Liste içini temizleme
+            Array.Clear(isimler, 0, isimler.Length);
+
+        Tekrar2:
+            Console.WriteLine((sayac + 1) + ". Öğrencinin ismi: " + ogrAdlari[sayac]);
+            sayac++;
+
+            if (sayac < ogrAdlari.Length) goto Tekrar2;
+
+
+            // Değer tipli değişkenleriçlerinde doğrudan veriyi tutarlar. Birbirlerine atandaıklarında da içlerindeki verinin kopyasını aktarırlar. Referans tipli değişkenler ise içlerinde veri değil verinin bulunduğu bellek adresini tutarlar. Dolayısıyla birbirlerine atandıklarında, içlerinde tuttukları bellek bölgesinin adresini kopyalamış olurlar. Böylece her 2 referans tipli değişken de aynı bellek adresini işaret eder ve değerini yönetir.
+            int sayi1 = 100;
+            int sayi2 = 100;
+
+            sayi1 = sayi2;
+            sayi2 = 500;
+
+            Console.WriteLine("Sayı1:" + sayi1 + " Sayı2:" + sayi2);
+
+
+            */
+
+            // =========================================================================================
+            // --------------------------    Kütüphane Konsol Otomasyonu    ----------------------------
+            // =========================================================================================
+
+            Console.Title = "KÜTÜPHANE OTOMASYONU";
+
+            string bookName;
+
+            //Kitap Dizisi
+            string[] books = {
+                "1984",
+                "Simyacı",
+                "Suç ve Ceza",
+                "Körlük",
+                "Sefiller",
+                "Hayvan Çiftliği",
+                "Dune",
+                "Küçük Prens",
+                "Cesur Yeni Dünya",
+                "Yeraltından Notlar"
+            };
+
+
+            // Arayüz
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hoş Geldiniz!");
+            Console.WriteLine();
+
+            Dongu:
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Aramak istediğiniz kitap ismini tam yazınız...");
+            Console.Write(">");
+            Console.ForegroundColor = ConsoleColor.White;
+            bookName = Console.ReadLine();
+
+            // İstenen kitabın indexini alma
+            int bookNumber = Array.IndexOf(books, bookName);
+
+            if(Array.IndexOf(books, bookName) == -1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("-----------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Aradığınız kitap kütüphanemizde bulunmamaktadır...");
+                Console.WriteLine("-----------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Kitap kütüphanemize bulunmaktadır.");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Sıra numarası: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(Array.IndexOf(books, bookName));
+                Console.WriteLine("-----------------------------------");
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Farklı bir kitap aramak ister misiniz? E=Evet , H=Hayır");
+            Console.ForegroundColor = ConsoleColor.White;
+            string devamMi = Console.ReadLine();
+
+            if(devamMi == "e" || devamMi == "E")
+            {
+                Console.ResetColor();
+                Console.Clear();
+                goto Dongu;
+            } 
+            else 
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Hoşçakalın..."); 
+            }
+
 
             Console.ReadKey();
         }
