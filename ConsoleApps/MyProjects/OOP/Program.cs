9ff4cbfad1deeeb8ff32d22fb827723a85bef025
mrtcnygt0 <mertcanyigit54@outlook.com>
2026-04-25 17:05:13 +0300

    OOP projesi eklendi

---
+++ ConsoleApps/MyProjects/OOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Araba
+    {
+
+    }
+
+    public class Product
+    {
+        public int ProductId;
+        public string ProductName;
+        public string ProductDescription;
+        public string ProductCategory;
+        public double ProductPrice;
+        public string ProductColor;
+        public string ProductSize;
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun = new Product();
+            urun.
+        }
+    }
+}
