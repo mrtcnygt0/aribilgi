63620e45edfceb8a922fcb4a4f55438ec242b0d6
Mert <mertcanyigit54@outlook.com>
2026-07-04 11:30:14 +0300

    Yeni

---
+++ WinForms/WinFormsADO/OkulADO/OgrGuncelle.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OkulADO
+{
+    public partial class OgrGuncelle : Form
+    {
+        public SqlConnection conn = new SqlConnection("Server=MERT\\SQLEXPRESS;Database=OgrenciDB;Integrated Security=True;Encrypt=False;");
+
+        SqlCommand cmdSelect = new SqlCommand("SELECT OgrNO, Ad, Soyad, Sinif, Sube, KayitTarih, Uyruk, Aciklama, Mezun FROM Ogrenciler");
+
+        SqlCommand cmdUpdate = new SqlCommand("UPDATE Ogrenciler SET Ad = @Ad, Soyad = @Soyad, Sinif = @Sinif, Sube = @Sube, KayitTarih = @KayitTarih, Uyruk = @Uyruk, Aciklama = @Aciklama, Mezun = @Mezun WHERE OgrNO = @OgrenciID");
+
+        private int secilenOgrenciID;
+
+        private void ListeyiYenile()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(cmdSelect.CommandText, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgvListe.DataSource = dt;
+
+        }
+
+        private void BtnReset()
+        {
+            btnKaydet.Enabled = true;
+            btnSil.Enabled = false;
+        }
+
+        private void Temizle()
+        {
+            txtAd.Clear();
+            txtSoyisim.Clear();
+            txtAciklama.Clear();
+            cmbMezun.SelectedIndex = 0;
+            cmbSinif.SelectedIndex = 0;
+            cmbSube.SelectedIndex = 0;
+            cmbUyruk.SelectedIndex = 0;
+            dtpTarih.Value = DateTime.Now;
+        }
+
+        private void Kontrol()
+        {
+            if (secilenOgrenciID == 0)
+            {
+                MessageBox.Show("Lütfen güncellemek istediğiniz öğrenciyi seçiniz.", "Eksik Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtAd.Text))
+            {
+                MessageBox.Show("Lütfen öğrencinin adını giriniz.", "Eksik Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtAd.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtSoyisim.Text))
+            {
+                MessageBox.Show("Lütfen öğrencinin soyadını giriniz.", "Eksik Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtSoyisim.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cmbSinif.Text))
+            {
+                MessageBox.Show("Lütfen sınıf seçiniz.", "Eksik Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbSinif.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cmbSube.Text))
+            {
+                MessageBox.Show("Lütfen şube seçiniz.", "Eksik Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbSube.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cmbUyruk.Text))
+            {
+                MessageBox.Show("Lütfen uyruk seçiniz.", "Eksik Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbUyruk.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cmbMezun.Text))
+            {
+                MessageBox.Show("Lütfen mezun bilgisini seçiniz.", "Eksik Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbMezun.Focus();
+                return;
+            }
+
+            DialogResult dugme = MessageBox.Show("Kayıt güncellenecek. Emin misiniz?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dugme == DialogResult.No)
+            {
+                return;
+            }
+
+            cmdUpdate.Connection = conn;
+            cmdUpdate.Parameters.Clear();
+            cmdUpdate.Parameters.AddWithValue("@Ad", txtAd.Text);
+            cmdUpdate.Parameters.AddWithValue("@Soyad", txtSoyisim.Text);
+            cmdUpdate.Parameters.AddWithValue("@Sinif", cmbSinif.SelectedItem);
+            cmdUpdate.Parameters.AddWithValue("@Sube", cmbSube.SelectedItem);
+            cmdUpdate.Parameters.AddWithValue("@KayitTarih", dtpTarih.Value);
+            cmdUpdate.Parameters.AddWithValue("@Uyruk", cmbUyruk.SelectedItem);
+            cmdUpdate.Parameters.AddWithValue("@Aciklama", txtAciklama.Text);
+            cmdUpdate.Parameters.AddWithValue("@Mezun", cmbMezun.SelectedItem);
+            cmdUpdate.Parameters.AddWithValue("@OgrenciID", secilenOgrenciID);
+
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+            }
+
+            cmdUpdate.ExecuteNonQuery();
+
+            if (conn.State != ConnectionState.Closed)
+            {
+                conn.Close();
+            }
+            MessageBox.Show("Kayıt başarıyla güncellendi.");
+
+            ListeyiYenile();
+            Temizle();
+            BtnReset();
+        }
+
+        public OgrGuncelle()
+        {
+            InitializeComponent();
+        }
+
+        private void OgrGuncelle_Load(object sender, EventArgs e)
+        {
+            ListeyiYenile();
+            BtnReset();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Kontrol();
+        }
+
+        private void dgvListe_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row = dgvListe.Rows[e.RowIndex];
+            row.Selected = true;
+
+            if (row.Cells[0].Value != null && row.Cells[0].Value.ToString() != string.Empty)
+            {
+                secilenOgrenciID = Convert.ToInt32(row.Cells[0].Value);
+                this.txtAd.Text = row.Cells[1].Value.ToString();
+                this.txtSoyisim.Text = row.Cells[2].Value.ToString();
+                this.cmbSinif.SelectedItem = row.Cells[3].Value.ToString().Trim();
+                this.cmbSube.SelectedItem = row.Cells[4].Value.ToString().Trim();
+                this.dtpTarih.Value = (DateTime)row.Cells[5].Value;
+                this.cmbUyruk.SelectedItem = row.Cells[6].Value.ToString().Trim();
+                this.txtAciklama.Text = row.Cells[7].Value.ToString();
+                this.cmbMezun.SelectedItem = row.Cells[8].Value.ToString().Trim();
+
+                btnSil.Enabled = true;
+            }
+        }
+
+        private void BtnAktif()
+        {
+            btnKaydet.Enabled = true;
+            btnSil.Enabled = true;
+        }
+
+        private void txtAd_TextChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void txtSoyisim_TextChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void cmbUyruk_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void cmbMezun_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void txtAciklama_TextChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void cmbSinif_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void cmbSube_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void dtpTarih_ValueChanged(object sender, EventArgs e)
+        {
+            BtnAktif();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            Temizle();
+        }
+    }
+}
---
+++ WinForms/WinFormsADO/OkulADO/OgrSil.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace OkulADO
+{
+    partial class OgrSil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OgrSil));
+            this.dgvListe = new System.Windows.Forms.DataGridView();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.chkOnay = new System.Windows.Forms.CheckBox();
+            this.lblOgrAd = new System.Windows.Forms.Label();
+            this.lblSecilenOgr = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListe
+            // 
+            this.dgvListe.AllowUserToAddRows = false;
+            this.dgvListe.AllowUserToDeleteRows = false;
+            this.dgvListe.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvListe.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dgvListe.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(46)))), ((int)(((byte)(45)))));
+            this.dgvListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListe.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvListe.Location = new System.Drawing.Point(0, 0);
+            this.dgvListe.MultiSelect = false;
+            this.dgvListe.Name = "dgvListe";
+            this.dgvListe.ReadOnly = true;
+            this.dgvListe.Size = new System.Drawing.Size(813, 344);
+            this.dgvListe.TabIndex = 30;
+            this.dgvListe.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvListe_CellClick);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.IndianRed;
+            this.btnSil.Enabled = false;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSil.Location = new System.Drawing.Point(282, 452);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(231, 68);
+            this.btnSil.TabIndex = 31;
+            this.btnSil.Text = "KAYDI SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(46, 411);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(755, 21);
+            this.richTextBox1.TabIndex = 32;
+            this.richTextBox1.Text = "* Bu işlemin geri dönüşü olmadığını biliyorum ve silme işleminin sorumluluklarını" +
+    " kabul ediyorum.";
+            // 
+            // chkOnay
+            // 
+            this.chkOnay.AutoSize = true;
+            this.chkOnay.Location = new System.Drawing.Point(15, 416);
+            this.chkOnay.Name = "chkOnay";
+            this.chkOnay.Size = new System.Drawing.Size(15, 14);
+            this.chkOnay.TabIndex = 33;
+            this.chkOnay.UseVisualStyleBackColor = true;
+            // 
+            // lblOgrAd
+            // 
+            this.lblOgrAd.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOgrAd.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblOgrAd.Location = new System.Drawing.Point(42, 363);
+            this.lblOgrAd.Name = "lblOgrAd";
+            this.lblOgrAd.Size = new System.Drawing.Size(141, 23);
+            this.lblOgrAd.TabIndex = 34;
+            this.lblOgrAd.Text = "Seçilen Öğrenci:";
+            // 
+            // lblSecilenOgr
+            // 
+            this.lblSecilenOgr.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecilenOgr.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblSecilenOgr.Location = new System.Drawing.Point(179, 363);
+            this.lblSecilenOgr.Name = "lblSecilenOgr";
+            this.lblSecilenOgr.Size = new System.Drawing.Size(324, 23);
+            this.lblSecilenOgr.TabIndex = 35;
+            this.lblSecilenOgr.Text = "Henüz seçim yapılmadı.";
+            // 
+            // OgrSil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(46)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(813, 532);
+            this.Controls.Add(this.lblSecilenOgr);
+            this.Controls.Add(this.lblOgrAd);
+            this.Controls.Add(this.chkOnay);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.dgvListe);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OgrSil";
+            this.Text = "OgrSil";
+            this.Load += new System.EventHandler(this.OgrSil_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListe;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox chkOnay;
+        private System.Windows.Forms.Label lblOgrAd;
+        private System.Windows.Forms.Label lblSecilenOgr;
+    }
+}
