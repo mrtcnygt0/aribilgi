9b0adc2bbb71a8b11512409a83bb6e36f6be783b
Mert <mertcanyigit54@outlook.com>
2026-08-19 07:58:09 +0300

    16.08.2026
    ASP.NET MVC Uygulaması Giriş

---
+++ MVC/MVC Web Apps/TestUygulamasi/Controllers/HesapController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestUygulamasi.Controllers
+{
+    public class HesapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string MesajVer()
+        {
+            return "Merhaba";
+        }
+
+        public double Topla()
+        {
+            return 155 + 255;
+        }
+
+        public IActionResult SonucGoster()
+        {
+            ViewBag.Toplam = Topla();
+            ViewBag.Mesaj = "Mert Can YİĞİT";
+            return View();
+        }
+    }
+}
--- Web/uygulama1/js/app.js
+++ Web/uygulama1/js/app.js
@@ -2,7 +2,24 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 
+var deger = parseInt(document.getElementById("deger").innerHTML);
+
 hamburger.addEventListener("click", () => {
   // ana sayfa, css, javascript menüleri altta görünsün veya kaybolsun
   menu.classList.toggle("show");
 });
+
+function btnEksi() {
+  deger = document.getElementById("deger").innerHTML = deger - 1;
+  console.log(deger);
+}
+
+function btnArti() {
+  deger = document.getElementById("deger").innerHTML = deger + 1;
+  console.log(deger);
+}
+
+function btnSifirla() {
+  deger = document.getElementById("deger").innerHTML = 0;
+  console.log(deger);
+}
